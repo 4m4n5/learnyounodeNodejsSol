b1627f355cf41e61586d53f405db9c041d69c982
Aman Shrivastava <aman.srivastava999@gmail.com>
2014-12-19 23:40:11 +0530

    Added 4 ex

---
+++ ex3.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+var buf = fs.readFileSync("/" + process.argv[2], 'utf8');
+var file = buf.toString();
+var noOfLines = file.split('\n');
+
+console.log(noOfLines.length - 1);
